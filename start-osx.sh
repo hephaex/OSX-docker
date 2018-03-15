c0540ea7552e185764ae9aaed6b2ff0ebc9507d9
Mario Cho <hephaex@gmail.com>
2018-03-16 01:11:58 +0900

    Update start-osx.sh

--- start-osx.sh
+++ start-osx.sh
@@ -6,14 +6,14 @@ then
 fi
 
 qemu-system-x86_64 -enable-kvm \
-  -m 3072 \
+  -m 4096 \
   -cpu Penryn,kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on \
   -machine pc-q35-2.9 \
   -smp 4,cores=2 \
   -usb -device usb-kbd -device usb-tablet \
   -device isa-applesmc,osk="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc" \
   -drive if=pflash,format=raw,readonly,file=OVMF_CODE-pure-efi.fd \
-  -drive if=pflash,format=raw,file=OVMF_VARS-pure-efi-1024x768.fd \
+  -drive if=pflash,format=raw,file=OVMF_VARS-pure-efi-1280x800.fd \
   -smbios type=2 \
   -device ich9-intel-hda -device hda-duplex \
   -device ide-drive,bus=ide.2,drive=Clover \
@@ -22,5 +22,5 @@ qemu-system-x86_64 -enable-kvm \
   -drive id=MacHDD,if=none,file='/data/mac_hdd.img',format=qcow2 \
   -device ide-drive,bus=ide.0,drive=MacDVD \
   -drive id=MacDVD,if=none,snapshot=on,media=cdrom,file='/data/osx.iso' \
-  -netdev user,id=net0 -device e1000-82545em,netdev=net0,id=net0,mac=52:54:00:c9:18:27 \
+  -netdev user,id=net0 -device e1000-82545em,netdev=net0,id=net0,mac=52:54:00:c8:18:28 \
   -vnc 0.0.0.0:0 
